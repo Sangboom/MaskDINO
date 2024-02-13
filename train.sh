61fd75f549453e1bb85f9d685c0fb362ff5e81d0
Sangboom <saturnbum@naver.com>
2024-02-13 16:48:57 +0900

    exp: add config

--- train.sh
+++ train.sh
@@ -4,4 +4,5 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 # python train_net.py --num-gpus 8 --config-file configs/coco/instance-segmentation/maskdino_R50_bs16_1x_3s.yaml --resume
 # python train_net.py --num-gpus 8 --config-file configs/coco/instance-segmentation/maskdino_R50_bs16_50ep_3s.yaml
 
-python train_net.py --num-gpus 8 --config-file configs/armbench/maskdino_R50_bs16_1x_3s_800.yaml
+python train_net.py --num-gpus 8 --config-file configs/armbench/maskdino_R50_bs16_1x_3s_multi.yaml
+python train_net.py --num-gpus 8 --config-file configs/armbench/maskdino_R50_bs16_1x_3s.yaml
